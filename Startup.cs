2355828ae3be14245b83a9215a370efc3e3e54eb
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-11 21:33:52 +1200

    create a css folder and site.site.css file to add some style to index.html

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace DutchTreat
 {
@@ -22,7 +16,7 @@ namespace DutchTreat
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             //middlewares - the order is important!
-            app.UseDefaultFiles(); 
+            app.UseDefaultFiles();
             app.UseStaticFiles();
         }
     }
