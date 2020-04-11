eeeb027d6bc79145c07995f3ced8a506e678d3d6
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-12 11:18:02 +1200

    create index.js and call it in index.html...
    hidden the form and create an event listener

---
+++ wwwroot/js/index.js
@@ -0,0 +1,12 @@
+﻿console.log("Hello Pluralsight!")
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", () => {
+    console.log("Buying Item");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children; 
