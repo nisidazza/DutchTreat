f736c8e330222e38b590cfbf3cc483308f9c59ee
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-12 12:43:07 +1200

    add function to hide/display login form

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -13,5 +13,11 @@
     productInfo.on("click", function () {
         console.log("You clicked on " + $(this).text());
     });
-});
 
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
+});
