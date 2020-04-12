d50f3f7fddb0b3b6aac6c11192b93b520d11b00e
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-12 12:07:35 +1200

    replace vanilla javascript with jquery in index.js

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,12 +1,17 @@
-﻿console.log("Hello Pluralsight!")
+﻿$(document).ready(function () {
+    console.log("Hello Pluralsight!");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", () => {
-    console.log("Buying Item");
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
 });
 
-var productInfo = document.getElementsByClassName("product-props");
-var listItems = productInfo.item[0].children; 
