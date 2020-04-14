d23bee603f90be05ebaf0095c890841950b8d687
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-14 15:08:18 +1200

    add bootstrap script and link in _Layout.cshtml

--- Services/NullMailService.cs
+++ Services/NullMailService.cs
@@ -9,11 +9,13 @@ namespace DutchTreat.Services
 {
     public class NullMailService : IMailService
     {
+        //field
         private readonly ILogger<NullMailService> _logger;
 
         // Inject a Logger into the class that we can use to log information
         public NullMailService(ILogger<NullMailService> logger)
         {
+
             _logger = logger;
         }
         public void SendMessage(string to, string subject, string body)
