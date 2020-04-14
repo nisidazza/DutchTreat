d1997cb071bc95dec8b4a11a9c4a649fb6fa4a3b
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-15 11:49:58 +1200

    setting-up configuration

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace DutchTreat
 {
@@ -13,9 +15,20 @@ namespace DutchTreat
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                //setup configuration
+                .ConfigureAppConfiguration(SetupConfiguration)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void SetupConfiguration(HostBuilderContext ctx, IConfigurationBuilder builder)
+        {
+            //Removing the default configuration options 
+            builder.Sources.Clear();
+            //configuration may exist in different format - the order matters!!!
+            builder.AddJsonFile("config.json", false, true)
+                   .AddEnvironmentVariables(); 
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@ using DutchTreat.Data;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,11 +11,21 @@ namespace DutchTreat
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DutchContext>();
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+            });
             //ENABLE RAZOR PAGES - 1/2
             services.AddRazorPages();
 
@@ -35,7 +47,7 @@ namespace DutchTreat
                 // Add Error Page
                 app.UseExceptionHandler("/error");
             }
-           
+
             //middlewares - the order is important!
             //app.UseDefaultFiles(); with MVC we don't need html file anymore
             app.UseStaticFiles();
@@ -45,14 +57,15 @@ namespace DutchTreat
             //this turns-on generic routing inside ASP.NET Core 3.1
             app.UseRouting();
 
-            app.UseEndpoints(cfg => {
+            app.UseEndpoints(cfg =>
+            {
                 //ENABLE RAZOR PAGES - 2/2
                 cfg.MapRazorPages();
 
                 cfg.MapControllerRoute("Fallback",
-                    "{controller}/{action}/{id?}", 
+                    "{controller}/{action}/{id?}",
                     new { controller = "App", action = "Index" }
-                    );                
+                    );
             });
         }
     }
