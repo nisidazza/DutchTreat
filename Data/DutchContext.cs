73d0e23224d7c664fb72e7450895868d8c9de3d7
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-15 14:40:11 +1200

    add some seeds by using HasData from EF Core 2.1

--- Data/DutchContext.cs
+++ Data/DutchContext.cs
@@ -16,5 +16,25 @@ namespace DutchTreat.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+
+        //this method specify how the mapping is going to happen between the entities and the database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Example:
+            //modelBuilder.Entity<Product>()
+            //    .Property(p => p.Title)
+            //    .HasMaxLength(50);
+
+            modelBuilder.Entity<Order>()
+                //add default or seeded data to our model - 
+                .HasData(new Order() // EF Core 2.1 introduce HasData - good for a limit number of data
+                {
+                    Id = 1,
+                    OrderDate = DateTime.UtcNow,
+                    OrderNumber = "12345"
+                });
+        }
     }
 }
---
+++ Data/Migrations/20200415023515_SeedData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DutchTreat.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "OrderDate", "OrderNumber" },
+                values: new object[] { 1, new DateTime(2020, 4, 15, 2, 35, 14, 843, DateTimeKind.Utc).AddTicks(7090), "12345" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
