9eccaf6585c6fe0a95b799cc4f620d7995954d16
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-17 14:48:52 +1200

    create ProductsController as API Controller - add an HttpGet to get all the products

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using DutchTreat.Data;
+using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    //Attributes
+    [Route("api/[Controller]")]
+    [ApiController]
+    [Produces("application/json")] // this API Controller is always going to return json
+    public class ProductsController : ControllerBase
+    {
+        private readonly IDutchRepository _repository;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(IDutchRepository repository, ILogger<ProductsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        //Expected response type
+        [ProducesResponseType(200)] 
+        [ProducesResponseType(400)]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            try
+            {
+                // Ok = 200 status code
+                return Ok(_repository.GetAllProducts());
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Failed to get products: {ex}");
+                return BadRequest("Failed to get products");
+            }
+            
+        }
+    }
+}
