34f5d324cd6a370caca80f17dbbd9b8fe1465fb0
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-21 14:25:24 +1200

    implement productList.component

--- ClientApp/shop/productList.component.ts
+++ ClientApp/shop/productList.component.ts
@@ -1,3 +1,19 @@
-﻿
+﻿import { Component } from "@angular/core";
 
-export class
+@Component({
+    selector: "product-list",
+    templateUrl: "productList.component.html",
+    styleUrls: []
+})
+export class ProductList {
+    public products = [{
+        title: "First Product",
+        price: 19.99
+    }, {
+        title: "Second Product",
+        price: 9.99
+    }, {
+        title: "Third Product",
+        price: 14.99
+    }];
+}
