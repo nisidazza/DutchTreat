e73f5508e7089b9c1a9eacec42fd5d3cc47f449f
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-21 15:48:22 +1200

    import Injectable as DataService may have its own dependencies

--- ClientApp/app/shared/dataService.ts
+++ ClientApp/app/shared/dataService.ts
@@ -1,7 +1,10 @@
 ﻿import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class DataService {
 
     constructor(private http: HttpClient) { }
+
     public products = [];
 }
