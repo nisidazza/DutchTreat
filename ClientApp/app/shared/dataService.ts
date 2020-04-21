7affa6c49f84318806b8dfc0d0c866ab6edf60b8
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-21 15:39:06 +1200

    remove sample data and import HttpClient

--- ClientApp/app/shared/dataService.ts
+++ ClientApp/app/shared/dataService.ts
@@ -1,14 +1,7 @@
-﻿
+﻿import { HttpClient } from '@angular/common/http';
 
 export class DataService {
-    public products = [{
-        title: "First Product",
-        price: 19.99
-    }, {
-        title: "Second Product",
-        price: 9.99
-    }, {
-        title: "Third Product",
-        price: 14.99
-    }];
+
+    constructor(private http: HttpClient) { }
+    public products = [];
 }
