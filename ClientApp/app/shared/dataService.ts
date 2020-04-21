cff172ffd6e0aecff01d56cbdaac268a99a0542c
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-22 11:10:50 +1200

    import Order in dataService.ts and create an addToOrder method

--- ClientApp/app/shared/dataService.ts
+++ ClientApp/app/shared/dataService.ts
@@ -3,11 +3,14 @@ import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
 import { map } from "rxjs/operators";
 import { Product } from './product';
+import { Order, OrderItem } from './order';
 
 @Injectable()
 export class DataService {
 
     constructor(private http: HttpClient) { }
+
+    public order: Order;
 
     public products: Product[] = [];
 
@@ -22,6 +25,25 @@ export class DataService {
                     return true;
                 })
             )
+    }
 
+    public addToOrder(newProduct: Product) {
+
+        if (this.order) {
+            this.order = new Order();
+        }
+
+        var item: OrderItem = new OrderItem();
+
+        item.productId = newProduct.id;
+        item.productArtist = newProduct.artist;
+        item.productArtId = newProduct.artId;
+        item.productCategory = newProduct.category;
+        item.productSize = newProduct.size;
+        item.productTitle = newProduct.title;
+        item.unitPrice = newProduct.price;
+        item.quantity = 1;
+
+        this.order.items.push(item);
     }
 }
