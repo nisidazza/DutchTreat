69ed779acfa83e9be3e67e8ba9f401af6f61b217
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-21 16:32:20 +1200

    call ngOnInit to get data from the API

--- ClientApp/app/shop/productList.component.ts
+++ ClientApp/app/shop/productList.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component } from "@angular/core";
+﻿import { Component, OnInit } from "@angular/core";
 import { DataService } from '../shared/dataService';
 
 @Component({
@@ -6,11 +6,23 @@ import { DataService } from '../shared/dataService';
     templateUrl: "productList.component.html",
     styleUrls: []
 })
-export class ProductList {
-    /** inject DataService **/
+export class ProductList implements OnInit {
+/** inject DataService **/
+    // // Called first time before the ngOnInit()
     constructor(private data: DataService) {
-        this.products = data.products;
     }
+
     //getting data from the service
     public products = [];
+
+     // Called after the constructor and called  after the first ngOnChanges() 
+    ngOnInit(): void {
+        this.data.loadProducts()
+            //get the data passed back in from the API
+            .subscribe(success => {
+                if (success) {
+                    this.products = this.data.products;
+                }
+            });
+    }
 }
