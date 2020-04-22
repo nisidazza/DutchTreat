2de32f3f14a555e90f1aa116c4e966a4904297d7
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-22 17:10:43 +1200

    create a login.component.ts and import it in app.module.ts

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -7,6 +7,8 @@ import { ProductList } from './shop/productList.component';
 import { Cart } from './shop/cart.component';
 import { Shop } from './shop/shop.component';
 import { Checkout } from './checkout/checkout.component';
+import { Login } from './login/login.component';
+
 import { DataService } from './shared/dataService';
 
 import { RouterModule } from '@angular/router';
@@ -28,7 +30,8 @@ let routes = [
         ProductList,
         Cart,
         Shop,
-        Checkout
+        Checkout,
+        Login
     ],
     imports: [
         BrowserModule,
