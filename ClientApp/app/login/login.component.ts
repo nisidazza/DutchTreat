1472c7298c4aa50b0fd83ed812f310fd27e56c67
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-22 17:22:50 +1200

    add a constructor in login.component.ts and add creds object that has username and password as properties

--- ClientApp/app/login/login.component.ts
+++ ClientApp/app/login/login.component.ts
@@ -1,4 +1,6 @@
 ﻿import { Component } from "@angular/core";
+import { DataService } from '../shared/dataService';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -7,5 +9,18 @@
 })
 
 export class Login {
+
+    constructor(private data: DataService, private router: Router) { }
+
+    //object that represents the data on the form
+    public creds = {
+        //untyped properties
+        username: "",
+        paswword: ""
+    }
+
+    onLogin() {
+      //Call the login Service
+    }
 
 }
