2e14b072a2e9f865acd1c504609812335602dd2d
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-23 11:25:59 +1200

    implement onLogin method in login.component.ts

--- ClientApp/app/login/login.component.ts
+++ ClientApp/app/login/login.component.ts
@@ -12,6 +12,7 @@ export class Login {
 
     constructor(private data: DataService, private router: Router) { }
 
+    errorMessage: string = "";
     //object that represents the data on the form
     public creds = {
         //untyped properties
@@ -21,8 +22,17 @@ export class Login {
 
     onLogin() {
         //Call the login Service
-        alert(this.creds.username);
-        this.creds.username += "!";
+        this.data.login(this.creds)
+            .subscribe(success => {
+                if (success) {
+                    if (this.data.order.items.length == 0) {
+                        this.router.navigate([""]);
+                    } else {
+                        this.router.navigate(["checkout"]);
+                    }
+                }
+            },
+                err => this.errorMessage = "Failed to login")
     }
 
 }
